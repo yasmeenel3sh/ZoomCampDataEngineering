d6f4ff35c3cc41f877e9d24e41a0b4bb0ab630e6
yasmeenel3sh <yasmeenel3sh@gmail.com>
2025-04-26 21:05:34 +0300

    Create basic terraform file

---
+++ docker_terraform/1_terraform_gcp/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.32.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "terraform-basics-458014"
+  region  = "europe-west1"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "terraform-basics-458014-terra-bucket"
+  location      = "EU"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
